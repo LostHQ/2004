280295ab6235ff27fe7196a2b5f7b6fb2994e005
halogod35 <hondadavey@gmail.com>
2025-08-27 17:32:06 -0400

    chore: updated run energy calc to new math

--- js/calculators/run_energy.js
+++ js/calculators/run_energy.js
@@ -1,9 +1,8 @@
 function runCalc() {
     const currentRunEnergy = parseInt(document.getElementById("currentRunEnergy").value);
     const agilityLevel = parseInt(document.getElementById("agilityLevel").value);
-    const recovered = Math.floor(agilityLevel / 9) + 8;
-    const recoveryPerTick = recovered / 50;
-    const secondsPerPercent = 0.6 / recoveryPerTick * 2;
+    const recovered = Math.floor(agilityLevel / 6) + 8; // every 9 levels in 225
+    const secondsPerPercent = 60 / recovered;
 
     document.getElementById("restorePerPercent").textContent = `${secondsPerPercent.toFixed(2)} seconds`;
 
