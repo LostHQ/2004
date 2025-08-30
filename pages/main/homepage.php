d7eebe5d303b3f7b98511e432f22fcc5aeaef171
halogod35 <hondadavey@gmail.com>
2025-08-29 21:01:44 -0400

    fix: standardized the front page layout

--- pages/main/homepage.php
+++ pages/main/homepage.php
@@ -1,19 +1,14 @@
 <?php
 function getPageContent() { return <<<HTML
-<table>
+<table class="main-page" style="padding-top: 15px;">
     <tbody>
-        <tr valign="top">
+        <tr>
             <td width="100">
                 <a href="?p=questguides">
                     <img src="img/questicon.png">
                 </a>
             </td>
             <td width="120">
-                <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
-                    <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;"></div>
-                </div>
-                <br>
-                <br>
                 Quest Guides<br>
                 <a href="?p=questguides" class="c">Click Here</a>
             </td>
@@ -24,57 +19,38 @@ function getPageContent() { return <<<HTML
                 </a>
             </td>
             <td width="120">
-                <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
-                    <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;"></div>
-                </div>
-                <br>
-                <br>
                 Skill Guides<br>
                 <a href="?p=skillguides" class="c">Click Here</a>
             </td>
         </tr>
-        <tr valign="top">
+        <tr>
             <td width="100">
                 <a href="?p=specialguides">
-                    <img src="img/genie.png" style="height:104px;margin-top:20px;">
+                    <img src="img/genie.png">
                 </a>
             </td>
             <td width="120">
-                <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
-                    <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;"></div>
-                </div>
-                <br>
-                <br>
                 Special Guides<br>
                 <a href="?p=specialguides" class="c">Click Here</a>
             </td>
             <td width="10"></td>
             <td width="100">
                 <a href="?p=itemdb">
-                    <img src="img/mm2_rs2b.jpg" style="width:80px;margin-top:20px;"></a>
+                    <img src="img/mm2_rs2b.jpg">
+                </a>
             </td>
             <td width="120">
-                <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
-                    <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;"></div>
-                </div>
-                <br>
-                <br>
                 Item Database<br>
                 <a href="?p=itemdb" class="c">Click Here</a>
             </td>
         </tr>
-        <tr valign="top">
+        <tr>
             <td width="100">
                 <a href="?p=droptables">
                     <img src="img/skeleton.png">
                 </a>
             </td>
             <td width="120">
-                <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
-                    <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;"></div>
-                </div>
-                <br>
-                <br>
                 Monster Drop Tables<br>
                 <a href="?p=droptables" class="c">Click Here</a>
             </td>
@@ -85,70 +61,48 @@ function getPageContent() { return <<<HTML
                 </a>
             </td>
             <td width="120">
-                <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
-                    <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;"></div>
-                </div>
-                <br>
-                <br>
                 Calculators<br>
                 <a href="?p=calculators" class="c">Click Here</a>
             </td>
         </tr>
-        <tr valign="top">
+        <tr>
             <td width="100">
-                <a href="?p=cluetables"><img src="img/casket1.png"></a>
+                <a href="?p=cluetables">
+                    <img src="img/casket1.png">
+                </a>
             </td>
-            
             <td width="120">
-                <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
-                    <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;"></div>
-                </div>
-                <br>
-                <br>
-                Clue Casket Tables<br>
+                Clue Casket<br>Tables<br>
                 <a href="?p=cluetables" class="c">Click Here</a>
             </td>
             <td width="10"></td>
             <td width="100">
                 <a href="?p=clueguides">
-                <img src="img/clueicon.png">
+                    <img src="img/clueicon.png">
                 </a>
             </td>
             <td width="120">
-                <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
-                    <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;"></div>
-                </div>
-                <br>
-                <br>
                 Treasure Trail Guides<br>
                 <a href="?p=clueguides" class="c">Click Here</a>
             </td>
         </tr>
-        <tr valign="top">
+        <tr>
             <td width="100">
                 <a href="?p=wallpapers">
-                    <img src="img/picture.png" style="width:80px;margin-top:20px;"></a>
+                    <img src="img/picture.png">
+                </a>
             </td>
             <td width="120">
-                <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
-                    <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;"></div>
-                </div>
-                <br>
-                <br>
                 Wallpapers<br>
                 <a href="?p=wallpapers" class="c">Click Here</a>
             </td>
             <td width="10"></td>
             <td width="100">
                 <a href="?p=">
-                    <img src="img/wip.png" style="width:80px;margin-top:20px;"></a>
+                    <img src="img/wip.png">
+                </a>
             </td>
             <td width="120">
-                <div style="LEFT: 0px; TOP: 0px; POSITION: relative;">
-                    <div style="LEFT: 0px; TOP: 0px; POSITION: absolute;"></div>
-                </div>
-                <br>
-                <br>
                 Work In Progress<br>
                 <a href="?p=" class="c">Click Here</a>
             </td>
