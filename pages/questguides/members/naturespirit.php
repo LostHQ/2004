f8abcf5202605b93071325495460f955063a26cb
halogod35 <hondadavey@gmail.com>
2025-09-03 09:42:06 -0400

    feat: added priest in peril and nature spirit for later

---
+++ pages/questguides/members/naturespirit.php
@@ -0,0 +1,80 @@
+<?php
+function getQuestGuide($questName, $questComplete) { return <<<HTML
+<h2>$questName</h2>
+<!-- <a href="#zombiemusiq" style="color: yellow; text-decoration: none; font-weight: bold;">ZombieMusiq Video Guide at bottom of page!</a> 
+<br><br> -->
+<b>Description:</b> After saving the holy man Drezel, he's seeking some assistance again.
+This time he has a special request for any adventurous sorts to search for the Druid 'Filliman Tarlock' and brave the terrors that infest the swamp of Mort Myre.
+<br><br>
+<b>Difficulty: <font color="Green">Novice</font></b>
+<br><br>
+<b>Length: <font color="Yellow">Medium</font></b><br>
+<h3>Required Quests:</h3>
+<ul style="list-style-type: none;">
+    <li><div data-progress>Priest In Peril</div><br></li>
+    <li><div data-progress>Restless Ghost</div><br></li>
+</ul>
+<h3>Items & Skills Needed:</h3>
+<ul style="list-style-type: none;">
+    <li><div data-progress>18 Crafting</div><br></li>
+    <li><div data-progress><canvas data-itemname="amulet_of_ghostspeak" data-size="25" data-show-label="inline"></canvas></div><br></li>
+    <li><div data-progress><canvas data-itemname="silver_bar" data-size="25" data-show-label="inline"></canvas></div><br></li>
+    <li><div data-progress><canvas data-itemname="sickle_mould" data-size="25" data-show-label="inline"></canvas></div><br></li>
+    <li><div data-progress>The ability to defeat a level 30 monster</div><br></li>
+</ul>
+<b>Starting Location:</b> Underground passageway of Sarodomin Temple (where you finish the Priest in Peril Quest)
+<br><br>
+<b>Reward:</b> 2 Quest points, 2,000 Defence XP, 2,000 Hitpoints XP, 3,000 Crafting XP, a Blessed Silver Sickle, and a Druid Pouch (and with it, the ability to kill Ghasts in swamp)
+<br><br>
+<hr>
+<h3>Instructions:</h3>
+<br>
+<div data-progress>Talk to Drezel (in the underground section entrence to Morytania).</div>
+<br><br>
+<div data-progress>He will explain that his friend, Filliman, has been missing and needs to be able to help out travelers.</div>
+<br><br>
+<div data-progress>Go south to the gate and click "No, just go through the gate".</div>
+<br><br>
+<div data-progress>Run (or walk, easier to have 100% energy) all the way down south-west until you see a small circular island. Run around to the end of it and click "Jump Bridge". If you fail, you will take some damage. (Note: higher agility seems to help you cross without falling, as with all other agility obstacles.)</div>
+<br><br>
+<div data-progress>Pick up the Washbowl and there will be a mirror under it, searching in the Grotto will allow you to find the Journal, sorry, I left this out!</div>
+<br><br>
+<div data-progress>Click on the black part of the grotto (so when enter grotto is selected) and Filiman comes out. You have to have your ammy of ghostspeak on. You have to convince him that he is a ghost, wich is finally done by using the mirror on him.</div>
+<br><br>
+<div data-progress>After he finishes talking, use the Journal (or "give" it to him) with him and he'll explain he wants to become a Nature Spirit and will need 3 things.</div>
+<br><br>
+<div data-progress>He will give you a spell and tell you that you need to be blessed. Go up to Drezel (where he is at the entrance to Morytania) and ask him to bless you. Note that he mentions that you have "Something of the Faith" about you.</div>
+<br><br>
+<div data-progress>Once you are blessed, go back down to the Grotto (Filliman's Home) and talk to him.</div>
+<br><br>
+<div data-progress>Walk north-west until you see some rotten logs on ground. Note that these are not items. Click "Cast Druid Spell" on the scroll Filiman gave to you when you are standing right next to a log and some fungi will appear on the log. Examine the used spell to solve the last of the puzzles.</div>
+<br><br>
+<div data-progress>Take the Fungi and go back to Filliman.</div>
+<br><br>
+<div data-progress>Use the Fungi with the west stone, the Used Druid Spell with the East Stone, and place yourself on the south stone. He will then ask you to enter the grotto (right-click the black part and click Enter Grotto).</div>
+<br><br>
+<div data-progress>Search the grotto and Filliman comes out. He will transform himself into a nature spirit. Then he will explain how to kill the Ghasts, by using a filled druid pouch on them, to materialize them. After that you can kill them normally. He will tell you that you need to make a Silver Sickle so he can bless it for you.</div>
+<br><br>
+<div data-progress>Go to Al Kharid Crafting Shop and buy a Silver Sickle Mould.</div>
+<br><br>
+<div data-progress>Use a Silver Bar with the Furnace and make the Silver Sickle. Making the Silver Sickle gives you 50 Crafting Exp. (thanks to Ltangel).</div>
+<br><br>
+<div data-progress>Go back to Filliman and talk to him in his Grotto.</div>
+<br><br>
+<div data-progress>He will bless the sickle and give you a Druid Pouch, telling you that you need to defeat three Ghasts to help him.</div>
+<br><br>
+<div data-progress>Make sure your Sickle is un-equipped, as you can now use it to cast the Bloom Spell.</div>
+<br><br>
+<div data-progress>Go get three more fungi, branch cuttings or golden pears and prepare to fight some Ghasts.</div>
+<br><br>
+<div data-progress>Kill three Ghasts and go back and talk to him.</div>
+<br><br>
+Quest Completed!
+$questComplete
+<!-- <span id="zombiemusiq"></span>
+<iframe width="450" height="253" src="https://www.youtube.com/embed/UjPa7n7Ymj8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<hr> -->
+This quest guide was written by Rekeri. Thanks to Sporhund, Stormer, ossie000, the_peleton, Ju Juitsu, Cricket55416, malku_raj, DRAVAN, trekkie, and SchmackyEvil for corrections.
+<br><br>
+This quest guide was entered into the RuneHQ.com database on Tue, Jul 13, 2004, at 02:18:08 PM by Pirate Bob49 and CJH and was last updated on Wed, Nov 02, 2005, at 08:37:48 PM by DRAVAN.
+HTML; }
---
+++ pages/questguides/members/priestinperil.php
@@ -0,0 +1,78 @@
+<?php
+function getQuestGuide($questName, $questComplete) { return <<<HTML
+<h2>$questName</h2>
+<!-- <a href="#zombiemusiq" style="color: yellow; text-decoration: none; font-weight: bold;">ZombieMusiq Video Guide at bottom of page!</a> 
+<br><br> -->
+<b>Description:</b> Recently contact has been lost with the temple to Saradomin on Misthalins' Eastern border. King Roald would be very interested to know what has caused this lack of communication, and seeks an adventurer willing to report back to him on the situation.
+<br><br>
+<b>Difficulty: <font color="Green">Novice</font></b>
+<br><br>
+<b>Length: <font color="Yellow">Medium</font></b><br>
+<h3>Required Quests:</h3>
+<ul style="list-style-type: none;">
+    <li><div data-progress>Rune Mysteries</div><br></li>
+</ul>
+<h3>Items & Skills Needed:</h3>
+<ul style="list-style-type: none;">
+    <li><div data-progress><canvas data-itemname="bucket_empty" data-size="25" data-show-label="inline"></canvas></div><br></li>
+    <li><div data-progress><canvas data-itemname="blankrune" data-size="25"></canvas>&nbsp;&nbsp;50 rune essence</div><br></li>
+    <li><div data-progress>The ability to defeat two level 30 enemies</div><br></li>
+</ul>
+<b>Starting Location:</b> King Roald in Varrock Palace
+<br><br>
+<b>Reward:</b> 1 Quest point, 1,406 Prayer XP, Wolfbane - a special silver dagger that prevents werewolves from shapeshifting into wolves when attacked, and access to a new town
+<br><br>
+<hr>
+<h3>Instructions:</h3>
+<br>
+<div data-progress>Start by talking to King Roald in Varrock Palace. He tells you that contact has been lost with the temple and wants you to go and check it out for him.</div>
+<br><br>
+<img src="img/questimages/priest1.png" width="450">
+<br><br>
+<div data-progress>The temple is located East of the Palace. Open the Gate and follow the trail to the temple. Knock on the door by right clicking on the door and clicking knock on. He will ask you if you will to help him by killing the dog. Tell him you will.</div>
+<br><br>
+<img src="img/questimages/priest2.png" width="450">
+<br><br>
+<div data-progress>Go down the dungeon entrance that is shown on the map to the north. Kill the Temple guardian Dog in the room.</div>
+<br><br>
+<img src="img/questimages/priest3.png" width="450">
+<br><br>
+Here is a picture of the Dungeon:
+<br><br>
+<img src="img/questimages/priest4.png">
+<br><br>
+<div data-progress>Go back and knock on the door again. He will act very strange. He suggests you talk to King Roald.</div>
+<br><br>
+<div data-progress>King Roald tells you that the dog was guarding the palace from attack, and he tells you to go back and fix it right away.</div>
+<br><br>
+<div data-progress>Go back to the temple. Kill a lvl 30 Monk of Zamorak. He will drop a golden key.</div>
+<br><br>
+<div data-progress>Go to the 3rd floor and talk to Drezel through cell door. Ask him to tell you about the Holy River. He will tell you how Sardomin made the river unpassable to all who were evil. He says that the river may not protect them again and asks you to help make it protect the lands again. He will tell you to get him out of the cell, incapacitate the vampire, and help fix the holy river.</div>
+<br><br>
+<img src="img/questimages/priest5.png" width="450">
+<br><br>
+<div data-progress>Go back to the dungeon and go through the gate. Study the monuments. When you find a monument with an iron key in it, trade the golden key for it.</div>
+<br><br>
+<div data-progress>Go back to the Cell and use the iron key on the cell door, then go in the cell and talk to Drezel. He will tell you that he cannot leave with the vampire still there, and tells you that the water of the river might be able to incapictate the vampire.</div>
+<br><br>
+<div data-progress>Go back to the monuments and use a bucket with the well in the middle of them.</div>
+<br><br>
+<div data-progress>Go back to Drezel and use the Water on the vampire. You will find out that the water is not blessed. Talk to Drezel and give the water to him and he will bless it for you.</div>
+<br><br>
+<div data-progress>Use the blessed water on the coffin, and talk to Drezel again. He will tell you that he will meet you down by the monument.</div>
+<br><br>
+<div data-progress>Go back to the monuments and go through the north-east gate. You will find Drezel there. Talk to him. He will tell you that the river has been unblessed. You suggest using rune essence to remove the Zamoraken magic. He tells you it will take 50 rune essence to do it. If you brought the unnoted essence, talk to him again and give it to him.</div>
+<br><br>
+Note: You MUST bring unnoted essence or he will not accept it.
+<br><br>
+<img src="img/questimages/priest6.png" width="450">
+<br><br>
+<div data-progress>You have now completed the quest, talk to the priest again and he will tell you about the underworld beyond the rift which will take you to a new land.</div>
+$questComplete
+<!-- <span id="zombiemusiq"></span>
+<iframe width="450" height="253" src="https://www.youtube.com/embed/UjPa7n7Ymj8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<hr> -->
+This quest guide was written by monkeymatt9. Thanks to xxtigurxx, chrisbarker, MuH-K0o0o, trekkie, faigel, and Gnat88 for corrections.
+<br><br>
+This quest guide was entered into the RuneHQ.com database on Tue, Jun 29, 2004, at 02:09:22 PM by stormer and CJH and was last updated on Tue, Jun 14, 2005, at 01:39:56 PM by Lewt04.
+HTML; }
