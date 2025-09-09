b82acb0da8d5852323520bd934a7ad6adf1c2bba
halogod35 <hondadavey@gmail.com>
2025-09-09 15:16:03 -0400

    chore: 2004Kit wording

--- pages/tools/main.php
+++ pages/tools/main.php
@@ -14,8 +14,12 @@ function getPageContent() {
         <img id="lightbox-img" src="">
     </div>
     <h3>2004Kit</h3>
-    SwiftSwitch/SwiftKit inspired toolkit for Lost City.
-    <br>
+    SwiftSwitch/SwiftKit inspired toolkit for Lost City, brought to you by Akg.
+    <br><br>
     <img src="img/tools/2004kit.png" width="450" onclick="openLightbox(this.src)">
+    <br><br>
+    Chat with other players via IRC and access all your favorite LostHQ guides and calculators directly in one streamlined app, giving you everything you need for Lost City in one place.
+    <br><br>
+    Downloads: <a href="downloads/2004kit/2004Kit.exe">Windows Executable</a>&nbsp;|&nbsp;<a href="">Linux AppImage</a>&nbsp;|&nbsp;<a href="">Source Code</a>
     <br><br>
 HTML; }
