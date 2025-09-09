2fe50edc63bfa24eaef7c568a97584b8242f5842
Collin_M <collin.marceaux@gmail.com>
2025-09-08 19:31:31 -0500

    simplify holiday theme some more

--- template/body.php
+++ template/body.php
@@ -2,22 +2,12 @@
 <table width="100%" height="100%" cellpadding="0" cellspacing="0"><tbody><tr>
 <td style="display:flex;justify-content:center;align-items:center;">
   <div style="LEFT: 0px; TOP: 0px; WIDTH: 600px; POSITION: relative;">
-    <?php if ($holiday) { ?>
       <table cellpadding="0" cellspacing="0"><tbody><tr></tr>
-        <td><img src="<?= $edge_a ?>" width="100" height="43" hspace="0" vspace="0"></td>
+        <td><img src="<?= $holiday ? $edge_a : 'img/edge_a.jpg' ?>" width="100" height="43" hspace="0" vspace="0"></td>
         <td><img src="img/edge_c.jpg" width="400" height="42" hspace="0" vspace="0"></td>
-        <td><img src="<?= $edge_d ?>" width="100" height="43" hspace="0" vspace="0"></td>
+        <td><img src="<?= $holiday ? $edge_d : 'img/edge_d.jpg' ?>" width="100" height="43" hspace="0" vspace="0"></td>
       </tr></tbody></table>
-      <table width="600" cellpadding="0" cellspacing="0" background="<?= $background ?>">
-    <?php } else { ?>
-      <table cellpadding="0" cellspacing="0"><tbody><tr></tr>
-        <td><img src="img/edge_a.jpg" width="100" height="43" hspace="0" vspace="0"></td>
-        <td><img src="img/edge_c.jpg" width="400" height="42" hspace="0" vspace="0"></td>
-        <td><img src="img/edge_d.jpg" width="100" height="43" hspace="0" vspace="0"></td>
-      </tr></tbody></table>
-      <table width="600" cellpadding="0" cellspacing="0" background="img/background2.jpg">
-    <?php } ?>
-
+      <table width="600" cellpadding="0" cellspacing="0" background="<?= $holiday ? $background : 'img/background2.jpg' ?>">
       <tbody>
         <tr>
           <td style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
