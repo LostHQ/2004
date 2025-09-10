fcb0bd880d154293705539de0b4e45521bcedf48
halogod35 <hondadavey@gmail.com>
2025-09-10 12:02:22 -0400

    feat: added minigames section + fishing trawler

--- pages/specialguides/main.php
+++ pages/specialguides/main.php
@@ -11,31 +11,66 @@ $speciallist = array(
     "statboosting" => "Stat Boosting"
 );
 
+$minigameslist = array(
+    // "agilityarena" => "Brimhaven Agility Arena",
+    // "castlewars" => "Castle Wars",
+    //+ "duelarena" => "Duel Arena",
+    "fishingtrawler" => "Fishing Trawler",
+    // "gamesroom" => "Burthorpe Games Room",
+    //+ "gnomeball" => "Gnome Ball",
+    //+ "rangingguild" => "Ranging Guild",
+    // "shadesofmort" => "Shades of Mort'ton"
+);
+
 function getExtraHeaderContent() { 
     return getJavaScriptVersion('js/equipment_interface.js'); 
 }
 
 function getPageContent() {
-    global $meta_data, $siteOptStyle, $speciallist;
+    global $meta_data, $siteOptStyle, $speciallist, $minigameslist;
     ob_start();
-    if (empty($_GET['guide'])) {
+
+    $guide = $_GET['guide'] ?? null;
+    $minigame = $_GET['minigames'] ?? null;
+
+    if (!$guide && !$minigame) {
         $meta_data['title'] = 'Special Guides';
         $meta_data['og:title'] = $meta_data['title'];
         $meta_data['og:image'] = 'img/genie.png';
         echo '<h3>Select the guide you would like to view a guide for:</h3>';
         echo renderSpecialList($speciallist);
-    } else {
-        $currSpecialGuide = htmlspecialchars($_GET['guide']);
+        echo '<br>';
+        echo renderMiniGamesList($minigameslist);
+        echo '<br>';
+    } elseif ($guide) {
+        $currSpecialGuide = htmlspecialchars($guide);
         $filePath = 'pages/specialguides/guides/' . $currSpecialGuide . '.php';
 
         if (file_exists($filePath)) {
             include $filePath;
-            $guideName = $speciallist[$currSpecialGuide];
+            $guideName = $speciallist[$currSpecialGuide] ?? ucfirst($currSpecialGuide);
             $meta_data['title'] = 'Special Guides > ' . $guideName;
             $meta_data['og:title'] = $meta_data['title'];
             $meta_data['og:image'] = 'img/genie.png';
 
             echo getSpecialGuide($guideName);
+        } else {
+            $stopload = true;
+            redirect("404");
+            exit;
+        }
+    } elseif ($minigame) {
+        $currMiniGame = htmlspecialchars($minigame);
+        $filePath = 'pages/specialguides/minigames/' . $currMiniGame . '.php';
+
+        if (file_exists($filePath)) {
+            include $filePath;
+            $gameName = $minigameslist[$currMiniGame] ?? ucfirst($currMiniGame);
+            $meta_data['title'] = 'Special Guides > ' . $gameName;
+            $meta_data['og:title'] = $meta_data['title'];
+            $meta_data['og:image'] = 'img/genie.png';
+
+            echo getSpecialGuide($gameName);
         } else {
             $stopload = true;
             redirect("404");
@@ -53,6 +88,19 @@ function renderSpecialList(array $speciallist): string {
     foreach ($speciallist as $specialKey => $specialName) {
         $url = "?p=specialguides&guide=" . urlencode($specialKey);
         $html .= '<tr><td><a href="' . $url . '">' . htmlspecialchars($specialName) . '</a></td></tr>';
+    }
+
+    $html .= '</tbody></table>';
+    return $html;
+}
+
+function renderMiniGamesList(array $minigameslist): string {
+    $html = '<table class="table" style="width: 40%;">';
+    $html .= '<thead><tr><th>Mini-Games</th></tr></thead><tbody>';
+
+    foreach ($minigameslist as $minigameKey => $minigameName) {
+        $url = "?p=specialguides&minigames=" . urlencode($minigameKey);
+        $html .= '<tr><td><a href="' . $url . '">' . htmlspecialchars($minigameName) . '</a></td></tr>';
     }
 
     $html .= '</tbody></table>';
---
+++ pages/specialguides/minigames/fishingtrawler.php
@@ -0,0 +1,48 @@
+<?php
+function getSpecialGuide($currSpecialGuide) { return <<<HTML
+<h2>$currSpecialGuide</h2>
+<h3>Introduction</h3>
+<table>
+    <tr>
+        <td>Trawler fishing is a fishing styled mini-game, which can be fun either solo or with team mates. It can be found in Port Khazard, north of Yanille and south of Ardougne.</td>
+        <td rowspan="2"><img src="img/special_guides/fishingtrawler/trawlerstart.png"></td>
+    </tr>
+    <tr>
+        <td>To start the mini-game, you should talk to Murphy at the docks. Please note that a Fishing level of 15 is required to do Trawler Fishing.</td>
+    </tr>
+</table>
+<br>
+In this mini-game you can catch any sea water fish up to your level, and it is the only way for high-level fisherman to catch Sea Turtles and Manta Rays.
+<h3>What You'll Need</h3>
+You should bring the following things when Trawler Fishing:
+<br><br>
+50 to 60 Swamp Paste. This can be made by collecting Swamp Tar at a swamp like Lumbridge Swamp, using it with flour, and then cooking the uncooked Swamp Tar over a fire. It can also be bought from the General Store at Port Khazard. Swamp Paste is used to repair leaks in the boat.
+<br><br>
+One Bailing Bucket. This can be bought at the Port Khazard General Store. It is used to bail water out of the bottom of the boat.
+<br><br>
+6 to 10 Ropes. These can also be bought at the Port Khazard General Store, as well as a number of other places. These are used to fix the fishing net.
+<br><br>
+NOTE: Bring friends, alone it is almost impossible!
+<h3>Trawler Fishing</h3>
+To start the mini-game, speak to Murphy at the Port Khazard docks and agree to help him. He will make sure a number of times, and then allow you to get onto the boat.
+<br><br>
+Once the first person gets on the boat, other team members will have one minute to get on as well.
+<br><br>
+Once you are aboard, you will need to start almost immediately. If the boat starts to leak, try and fill it immediately by clicking on it. Each time you fill a leak you will use one Swamp Paste. When there is a leak, the boat will begin to fill with water. You can tell how much water is in the boat by the bar at the top of your screen. Keep bailing to keep the bar down.
+<br><br>
+<img src="img/special_guides/fishingtrawler/trawlerleaks.png" width="450">
+<br><br>
+At the top of the screen, you will also be able to see the status of the net. If it is ripped you will need to go to the rear of the boat and climb up the ladder. Click on the net and you'll fix it, and one rope will disappear from your inventory.
+<br><br>
+<img src="img/special_guides/fishingtrawler/trawlernet.png" width="450">
+<br><br>
+To bail water out of the boat, click on the bailing bucket to fill it up, and then click again to get rid of the water. If the boat gets full of water, you will sink and end up in the water. To get out of the water, click on a floating barrel and you will end up at Port Sarim, and the boat will again be waiting for you at Port Khazard.
+<br><br>
+<img src="img/special_guides/fishingtrawler/trawlersunk.png">
+<br><br>
+Once ten minutes is up, the mini-game is over. When you get back, inspect the net. Here you can choose what you would like to keep, and leave the rest.
+<hr>
+This Mini-Game guide was written by Downstrike. Thanks to kngkyle for corrections.
+<br><br>
+This Mini-Game guide was entered into the RuneHQ.com database on Sun, Oct 19, 2003, at 12:00:00 AM by MrStormy and was last updated on Sun, Sep 11, 2005, at 10:51:15 AM by Fireball0236.
+HTML; }
