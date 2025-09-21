3217124e99a0344c748dcafaedc7c5e7f2076e14
halogod35 <hondadavey@gmail.com>
2025-09-21 18:42:35 -0400

    fix: vampire slayer guide fixes (thanks Fran 2004)

--- pages/questguides/free/vampireslayer.php
+++ pages/questguides/free/vampireslayer.php
@@ -19,19 +19,23 @@ function getQuestGuide($questName, $questComplete) { return <<<HTML
 <hr>
 <h3>Instructions:</h3>
 <br>
-<div data-progress>Find Morgan in Draynor. It's not that hard; it's just a small village. He'll tell you about the vampire and ask you to kill it. He'll also tell you to go find his friend Dr. Harlow at the Jolly Boar Inn.</div>
+<div data-progress>Find Morgan in Draynor; he has the quest symbol above his house. He'll tell you about the vampire and ask you to kill it. He'll also tell you to go find his friend, Dr. Harlow, at the Jolly Boar Inn.</div>
 <br><br>
-<div data-progress>First, go upstairs and search the wall cupboard for a clove of garlic. Then go northeast of Varrock to the inn.</div>
+<div data-progress>Go upstairs in his house and search the west wall cupboard for a clove of garlic.</div>
 <br><br>
-<div data-progress>Talk to him and get him the beer he asks for. He'll tell you how to beat it and give you a stake.</div>
+<div data-progress>Head to Varrock and go to the Jolly Boar Inn, which is northeast outside the city walls.</div>
 <br><br>
-<div data-progress>Get everything you need and go to Draynor Manor. Head to the stairs that go down. Open the coffin and the Count should jump out and start attacking you.</div>
+<div data-progress>Talk to Dr. Harlow and get him the beer that he asks for. He'll tell you how to defeat the vampire. He then hands you a stake to hammer it through the vampire's undead heart.</div>
 <br><br>
-<div data-progress>Attack him until he has a full red bar, and the stake should automatically stab him. If not, make sure you have a hammer and the stake with you. If it still doesn't work, try using garlic on him or manually stabbing him with the stake.</div>
+<div data-progress>When you are ready, make sure you have the garlic, stake, and hammer in inventory, and go to Draynor Manor. Head to the stairs that go down on the east side of the manor.</div>
+<br><br>
+<div data-progress>Open the coffin, and the Count should jump out and start attacking you.</div>
+<br><br>
+<div data-progress>Attack him until he has a full red bar, and  then use the stake on him to stab him through the heart. If not, make sure you have a hammer and the stake with you.</div>
 <br><br>
 Once he's dead, you have finished the quest. (Congratulations!)
 $questComplete
-This quest guide was written on RuneHQ by Gnat88. Thanks to Weezy and stormer for corrections.
+This quest guide was written on RuneHQ by Gnat88. Thanks to Weezy, stormer, Fran 2004 for corrections.
 <br><br>
-This quest guide was entered into the database on Sat, Feb 28, 2004, at 06:37:37 PM by Monkeychris and was last updated on Sun, Apr 11, 2004, at 02:25:25 PM.
+This quest guide was entered into the database on Sat, Feb 28, 2004, at 06:37:37 PM by Monkeychris and was last updated on Sun, Apr 11, 2004, at 06:42:25 PM by Halogod35.
 HTML; }
