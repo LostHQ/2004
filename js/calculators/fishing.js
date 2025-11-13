cf26a1e539b5c4c9d2968e16efcf28735190b862
Collin_M <collin.marceaux@gmail.com>
2025-11-12 19:53:12 -0600

    fix: fishing calc use debugnames

--- js/calculators/fishing.js
+++ js/calculators/fishing.js
@@ -4,23 +4,23 @@ function runCalc() {
     const xpNeeded = targetXP - currentXP;
 
     const fishes = [
-        { name: "Shrimp", xp: 10, level: 1 },
-        { name: "Sardine", xp: 20, level: 5 },
-        { name: "Herring", xp: 30, level: 10 },
-        { name: "Anchovies", xp: 40, level: 15 },
-        { name: "Mackerel", xp: 20, level: 16 },
-        { name: "Trout", xp: 50, level: 20 },
-        { name: "Cod", xp: 45, level: 23 },
-        { name: "Pike", xp: 60, level: 25 },
-        { name: "Salmon", xp: 70, level: 30 },
-        { name: "Tuna", xp: 80, level: 35 },
-        { name: "Lobster", xp: 90, level: 40 },
-        { name: "Bass", xp: 100, level: 46 },
-        { name: "Swordfish", xp: 100, level: 50 },
-        { name: "Lava Eel", xp: 60, level: 53 },
-        { name: "Shark", xp: 110, level: 76},
-        { name: "SeaTurtle", xp: 57, level: 79 },
-        { name: "MantaRay", xp: 69, level: 81 }
+        { name: "raw_shrimp", xp: 10, level: 1 },
+        { name: "raw_sardine", xp: 20, level: 5 },
+        { name: "raw_herring", xp: 30, level: 10 },
+        { name: "raw_anchovies", xp: 40, level: 15 },
+        { name: "raw_mackerel", xp: 20, level: 16 },
+        { name: "raw_trout", xp: 50, level: 20 },
+        { name: "raw_cod", xp: 45, level: 23 },
+        { name: "raw_pike", xp: 60, level: 25 },
+        { name: "raw_salmon", xp: 70, level: 30 },
+        { name: "raw_tuna", xp: 80, level: 35 },
+        { name: "raw_lobster", xp: 90, level: 40 },
+        { name: "raw_bass", xp: 100, level: 46 },
+        { name: "raw_swordfish", xp: 100, level: 50 },
+        { name: "raw_lava_eel", xp: 60, level: 53 },
+        { name: "raw_shark", xp: 110, level: 76 },
+        { name: "raw_seaturtle", xp: 57, level: 79 },
+        { name: "raw_mantaray", xp: 69, level: 81 }
     ];
     
     updateProgressBar(currentXP, targetXP);
@@ -34,7 +34,7 @@ function runCalc() {
         let row = document.createElement("tr");
         row.innerHTML = `
             <td>${fish.level}</td>
-            <td><canvas data-itemname="raw_${fish.name.toLowerCase().replace(/\s+/g, "_")}" data-show-label="inline"></canvas></td>
+            <td><canvas data-itemname="${fish.name}" data-show-label="inline"></canvas></td>
             <td>${fish.xp}</td>
             <td>${estimatedCatches.toLocaleString()}</td>
         `;
