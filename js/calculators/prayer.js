42884edc850903870c837bd8f93df98a43c54f3e
Collin_M <collin.marceaux@gmail.com>
2025-12-30 19:41:04 -0600

    update combat calculator to use monster_drops for up-to-date NPC info

--- js/calculators/prayer.js
+++ js/calculators/prayer.js
@@ -1,30 +1,30 @@
+export const bonesXp = {
+    bones: 4.5,
+    bones_burnt: 4.5,
+    bat_bones: 4.5,
+    wolf_bones: 4.5,
+    big_bones: 15,
+    babydragon_bones: 30,
+    dragon_bones: 72,
+};
+
 function runCalc() {
     const currentXP = parseInt(document.getElementById("currentXP").value);
     const targetXP = parseInt(document.getElementById("targetXP").value);
     const xpNeeded = targetXP - currentXP;
-
-    const bones = {
-        Bones: 4.5,
-        "bones burnt": 4.5,
-        "Bat Bones": 4.5,
-        "Wolf Bones": 4.5,
-        "Big Bones": 15,
-        "Babydragon Bones": 30,
-        "Dragon Bones": 72,
-    };
 
     updateProgressBar(currentXP, targetXP);
 
     const tableBody = document.querySelector("#resultsTable tbody");
     tableBody.innerHTML = "";
 
-    for (let bone in bones) {
-        let boneCount = Math.ceil(xpNeeded / bones[bone]);
+    for (let bone in bonesXp) {
+        let boneCount = Math.ceil(xpNeeded / bonesXp[bone]);
 
         let row = document.createElement("tr");
         row.innerHTML = `
             <td><canvas data-itemname="${bone.toLowerCase().replace(/\s+/g, "_")}" data-show-label="inline"></canvas></td>
-            <td>${bones[bone]}</td>
+            <td>${bonesXp[bone]}</td>
             <td>${boneCount.toLocaleString()}</td>
         `;
         tableBody.appendChild(row);
