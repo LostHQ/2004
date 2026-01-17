91a2a5df8868e0121085b53a40abd4bdf33f4247
Collin_M <collin.marceaux@gmail.com>
2026-01-16 21:17:26 -0600

    update thieving calc to use sprite loader

--- js/calculators/thieving.js
+++ js/calculators/thieving.js
@@ -4,36 +4,36 @@ function setMode(newMode) {
     runCalc();
 }
 const npcs = {
-    "Man/Woman": { xp: 8, level: 1 },
-    "Digsite Workman": { xp: 10.4, level: 10 },
-    Farmer: { xp: 14.5, level: 10 },
-    "Warrior/Al Kharid warrior": { xp: 26, level: 25 },
-    Rogue: { xp: 36.5, level: 32 },
-    Guard: { xp: 46.8, level: 40 },
-    "Knight of Ardougne": { xp: 84.3, level: 55 },
-    "Yanille Watchman": { xp: 137.5, level: 65 },
-    Paladin: { xp: 151.8, level: 70 },
-    Gnome: { xp: 198.3, level: 75 },
-    Hero: { xp: 273.3, level: 80 },
+    "Man/Woman": { xp: 8, level: 1, npcname: "man" },
+    "Digsite Workman": { xp: 10.4, level: 10, npcname: "digworkman1" },
+    "Farmer": { xp: 14.5, level: 10, npcname: "farmer1" },
+    "Warrior/Al Kharid warrior": { xp: 26, level: 25, npcname: "warrior_woman" },
+    "Rogue": { xp: 36.5, level: 32, npcname: "rogue" },
+    "Guard": { xp: 46.8, level: 40, npcname: "guard1" },
+    "Knight of Ardougne": { xp: 84.3, level: 55, npcname: "knight_of_ardougne" },
+    "Yanille Watchman": { xp: 137.5, level: 65, npcname: "yanille_watchman" },
+    "Paladin": { xp: 151.8, level: 70, npcname: "paladin" },
+    "Gnome": { xp: 198.3, level: 75, npcname: "gnome" },
+    "Hero": { xp: 273.3, level: 80, npcname: "hero" },
 };
 
 const stalls = {
-    "Bakery Stall": { xp: 16, level: 5 },
-    "Tea Stall": { xp: 16, level: 5 },
-    "Silk Stall": { xp: 24, level: 20 },
-    "Fur Stall": { xp: 36, level: 35 },
-    "Silver Stall": { xp: 54, level: 50 },
-    "Spice Stall": { xp: 81, level: 65 },
-    "Gem Stall": { xp: 16, level: 75 },
+    "Bakery Stall": { xp: 16, level: 5, itemname: "bread" },
+    "Tea Stall": { xp: 16, level: 5, itemname: "cup_of_tea" },
+    "Silk Stall": { xp: 24, level: 20, itemname: "silk" },
+    "Fur Stall": { xp: 36, level: 35, itemname: "fur" },
+    "Silver Stall": { xp: 54, level: 50, itemname: "silver_ore" },
+    "Spice Stall": { xp: 81, level: 65, itemname: "spicespot" },
+    "Gem Stall": { xp: 16, level: 75, itemname: "sapphire" },
 };
 
 const chests = {
-    "10 Coin Chest": { xp: 7.8, level: 1 },
-    "Nature Rune Chest": { xp: 25, level: 28 },
-    "50 Coin Chest": { xp: 125, level: 43 },
-    "Steel Arrowtips Chest": { xp: 150, level: 47 },
-    "Blood Rune Chest": { xp: 250, level: 59 },
-    "Ardougne Castle Chest": { xp: 500, level: 72 },
+    "10 Coin Chest": { xp: 7.8, level: 1, itemname: ["coins",10] },
+    "Nature Rune Chest": { xp: 25, level: 28, itemname: ["naturerune",1] },
+    "50 Coin Chest": { xp: 125, level: 43, itemname: ["coins",50] },
+    "Steel Arrowtips Chest": { xp: 150, level: 47, itemname: ["steel_arrowheads",15] },
+    "Blood Rune Chest": { xp: 250, level: 59, itemname: ["bloodrune",1] },
+    "Ardougne Castle Chest": { xp: 500, level: 72, itemname: ["shark",1] },
 };
 
 const pickabledoors = {
@@ -70,27 +70,39 @@ function runCalc() {
     }
 
     for (const [source, data] of Object.entries(dataSet)) {
-        var lockpick = "";
-        if (dataSet == pickabledoors) {
-            if (data.lockpick) {
-                var lockpick = "<br>(requires lockpick)";
-            }
-            if (source.endsWith("Door")) {
-                var img = `<img src="img/calculators/thieving/door.png" height=32px alt="${source}">`;
-            } else {
-                var img = `<img src="img/calculators/thieving/gate.png" height=32px alt="${source}">`;
-            }
-        } else {
-            var img = `<img src="img/calculators/thieving/${source.toLowerCase().replace(/\s+/g, "_").replace("/", "_")}.png" height=32px alt="${source}">`;
+        var img = "";
+        switch (dataSet) {
+            default:
+            case chests:
+                img = `<canvas itemname="${data.itemname[0]}" amount="${data.itemname[1]}" name-replace="${source}" show-label="inline"></canvas>`;
+                break;
+            case stalls:
+                img = `<canvas itemname="${data.itemname}" name-replace="${source}" show-label="inline"></canvas>`;
+                break;
+            case npcs:
+                img = `<canvas npcname="${data.npcname}" icon-size="64"></canvas><br>${source}`;
+                break;
+            case pickabledoors:
+                var lockpick = "";
+                if (data.lockpick) {
+                    lockpick = "<br>(requires lockpick)";
+                }
+                if (source.endsWith("Door")) {
+                    var img = `<img src="/img/calculators/thieving/door.png" height=32px alt="${source}"><br>${source}&nbsp;${lockpick}`;
+                } else {
+                    var img = `<img src="/img/calculators/thieving/gate.png" height=32px alt="${source}"><br>${source}&nbsp;${lockpick}`;
+                }
+                break;
         }
         const count = Math.ceil(xpNeeded / data.xp);
         const row = document.createElement("tr");
         row.innerHTML = `
             <td>${data.level}</td>
-            <td><div class="centered-content">${img}&nbsp;${source}${lockpick}</div></td>
+            <td>${img}</td>
             <td>${data.xp}</td>
             <td>${count.toLocaleString()}</td>
         `;
         tableBody.appendChild(row);
     }
+    window.safeRenderAllSprites();
 }
