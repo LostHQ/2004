80a42199255fe29294ee19a09b3b34d49a1ec12c
Collin_M <24929130+Collin-M@users.noreply.github.com>
2026-05-23 17:43:56 -0500

    274 fishing

--- js/calculators/fishing.js
+++ js/calculators/fishing.js
@@ -1,5 +1,6 @@
 const fishes = {
     raw_shrimp: { xp: 10, level: 1 },
+    tbwt_raw_karambwanji: { xp: 5, level: 5 },
     raw_sardine: { xp: 20, level: 5 },
     raw_herring: { xp: 30, level: 10 },
     raw_anchovies: { xp: 40, level: 15 },
@@ -7,15 +8,17 @@ const fishes = {
     raw_trout: { xp: 50, level: 20 },
     raw_cod: { xp: 45, level: 23 },
     raw_pike: { xp: 60, level: 25 },
+    mort_slimey_eel: { xp: 65, level: 28 },
     raw_salmon: { xp: 70, level: 30 },
     raw_tuna: { xp: 80, level: 35 },
     raw_lobster: { xp: 90, level: 40 },
     raw_bass: { xp: 100, level: 46 },
     raw_swordfish: { xp: 100, level: 50 },
     raw_lava_eel: { xp: 60, level: 53 },
+    tbwt_raw_karambwan: { xp: 105, level: 65 },
     raw_shark: { xp: 110, level: 76 },
-    raw_seaturtle: { xp: 57, level: 79 },
-    raw_mantaray: { xp: 69, level: 81 },
+    //raw_seaturtle: { xp: 57, level: 79 }, (only available from trawler right now)
+    //raw_mantaray: { xp: 69, level: 81 }, (only available from trawler right now)
 };
 
 function runCalc() {
@@ -31,8 +34,7 @@ function runCalc() {
     for (const [item, data] of Object.entries(fishes)) {
         const estimatedCatches = Math.ceil(xpNeeded / data.xp);
         const row = document.createElement("tr");
-        row.innerHTML =
-            `<td>${data.level}</td>
+        row.innerHTML = `<td>${data.level}</td>
             <td><canvas itemname="${item}" show-label="inline"></canvas></td>
             <td>${data.xp}</td>
             <td>${estimatedCatches.toLocaleString()}</td>`;
