744a9eb3b3571a9d1177cd708936516e2698afe0
Collin_M <24929130+Collin-M@users.noreply.github.com>
2026-05-22 22:12:26 -0500

    fix: tbwt needs 4 total agility doses

--- pages/questguides/members/tbwt.php
+++ pages/questguides/members/tbwt.php
@@ -16,7 +16,7 @@ function getQuestGuide($questName, $questComplete) { return <<<HTML
     <ul style="list-style-type: none;">
         <li><div data-progress>30 Cooking</div><br></li>
         <li><div data-progress>15 Agility</div><br></li>
-        <li><div data-progress><canvas itemname="4dose1agility" icon-size="25" show-label="inline" amount="2"></canvas></div></li>
+        <li><div data-progress><canvas itemname="4dose1agility" icon-size="25" show-label="inline" name-append=" (or 4 total doses)"></canvas></div></li>
         <li><div data-progress><canvas itemname="pestle_and_mortar" icon-size="25" show-label="inline"></canvas></div></li>
         <li><div data-progress><canvas itemname="iron_spear" icon-size="25" show-label="inline" name-replace="Iron Spear (or higher tier)"></canvas></div></li>
         <li><div data-progress><canvas itemname="karamja_rum" icon-size="25" show-label="inline" name-append=" (purchase at any bar on Karamja)"></canvas></div></li>
