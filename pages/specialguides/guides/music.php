37dae3298b8c3ecf4eab9f4550c555a396337948
Halogod35 <hondadavey@gmail.com>
2026-06-01 16:42:15 -0400

    chore: 274 music

--- pages/specialguides/guides/music.php
+++ pages/specialguides/guides/music.php
@@ -47,7 +47,7 @@ Quick Links<br>
     <tr><td>Arabique</td><td>At the start of Taverley Dungeon</td></tr>
     <tr><td>Army Of Darkness</td><td>Skeleton Coal Mine and Wilderness Castle area and in the Chaos Altar Temple</td></tr>
     <tr><td>Arrival</td><td>Area south of Taverley between lake and member fence</td></tr>
-    <!-- <tr><td>Artistry</td><td>Mime Random Event</td></tr> -->
+    <tr><td>Artistry</td><td>Mime Random Event</td></tr>
     <tr><td>Attack1</td><td>Around the Khazard Battlefield</td></tr>
     <tr><td>Attack2</td><td>In Karamja/Crandor Dungeon</td></tr>
     <tr><td>Attack3</td><td>Poison spider-filled teleport area to the KBD Lair</td></tr>
@@ -63,13 +63,13 @@ Quick Links<br>
     <tr><td>Background2</td><td>On Entrana</td></tr>
     <tr><td>Ballad Of Enchantment</td><td>Around Clock Tower</td></tr>
     <tr><td>Bandit Camp</td><td>In The Bebadin Camp</td></tr>
-    <!-- <tr><td>Barbarianism</td><td>Barbarian Village</td></tr> -->
+    <tr><td>Barbarianism</td><td>Barbarian Village</td></tr>
     <tr><td>Baroque</td><td>Ardougne Market</td></tr>
     <tr><td>Beyond</td><td>Underground Dwarf passage under White Wolf Mountain including the Ice Queen's cave</td></tr>
     <tr><td>Big Chords</td><td>Around the Yanille Watchtower and West Yanille</td></tr>
     <tr><td>Book Of Spells</td><td>Lumbridge Swamp and west of the Mage Arena</td></tr>
-    <!-- <tr><td>Borderland</td><td>East of Relleka</td></tr> -->
-    <!-- <tr><td>Breeze</td><td>Within the Elven nation</td></tr> -->
+    <tr><td>Borderland</td><td>East of Relleka</td></tr>
+    <tr><td>Breeze</td><td>Within the Elven nation</td></tr>
     <tr>
         <th colspan="2"><span id="c"></span>C</th>
     </tr>
@@ -81,11 +81,11 @@ Quick Links<br>
     <tr><td>Chain Of Command</td><td>Temple of Ikov</td></tr>
     <tr><td>Chompy Hunt</td><td>Southeast of Ogre Village at "Big Chompy Bird Hunting" quest start</td></tr>
     <tr><td>Close Quarters</td><td>Red Spider ruins at level 26-29 Wilderness</td></tr>
-    <!-- <tr><td>Competition</td><td>Burthorpe Game Room</td></tr> -->
-    <!-- <tr><td>Complication</td><td>Chaos Rune Altar</td></tr> -->
+    <tr><td>Competition</td><td>Burthorpe Game Room</td></tr>
+    <tr><td>Complication</td><td>Chaos Rune Altar</td></tr>
     <tr><td>Contest</td><td>Death Plateau</td></tr>
-    <!-- <tr><td>Courage</td><td>Start of Taverley Dungeon</td></tr> -->
-    <!-- <tr><td>Crystal Castle</td><td>Near Prifddinas Gate</td></tr> -->
+    <tr><td>Courage</td><td>Start of Taverley Dungeon</td></tr>
+    <tr><td>Crystal Castle</td><td>Near Prifddinas Gate</td></tr>
     <tr><td>Crystal Cave</td><td>Water Rune Altar</td></tr>
     <tr><td>Crystal Sword</td><td>Low Level Wilderness North of Varrock and also in the East Ardougne Dungeon</td></tr>
     <tr><td>Cursed</td><td>Right when you enter the Underground Passage</td></tr>
@@ -93,14 +93,14 @@ Quick Links<br>
         <th colspan="2"><span id="d"></span>D</th>
     </tr>
     <tr><td>Dangerous</td><td>Around the Wilderness Poison Wood</td></tr>
-    <!-- <tr><td>Dangerous Road</td><td>Karamja Volcano Dungeon, Karamja side Dark - At the giants in Edgeville Dungeon also around the Giants pass around level 25 East Wilderness</td></tr> -->
+    <tr><td>Dangerous Road</td><td>Karamja Volcano Dungeon, Karamja side Dark - At the giants in Edgeville Dungeon also around the Giants pass around level 25 East Wilderness</td></tr>
     <tr><td>Dark2</td><td>Wilderness at Ghosts And Giants</td></tr>
     <tr><td>Dead Quiet</td><td>Entrance to Mort Myre</td></tr>
-    <!-- <tr><td>Deadlands</td><td>East of Canifis</td></tr> -->
+    <tr><td>Deadlands</td><td>East of Canifis</td></tr>
     <tr><td>Deep Wildy</td><td>Around the level 38 Wilderness Evil Altar and North of Edgeville</td></tr>
     <tr><td>Desert Voyage</td><td>Around the Tourist Trap Mining Camp</td></tr>
     <tr><td>Doorways</td><td>North Varrock Forest</td></tr>
-    <!-- <tr><td>Down to Earth</td><td>In the Earth Rune Altar</td></tr> -->
+    <tr><td>Down to Earth</td><td>In the Earth Rune Altar</td></tr>
     <tr><td>Dream1</td><td>Between Lumbridge and Draynor</td></tr>
     <tr><td>Duel Arena</td><td>Duel Arena</td></tr>
     <tr><td>Dunjun</td><td>Taverley Dungeon right after the Magic Axe room</td></tr>
@@ -108,20 +108,20 @@ Quick Links<br>
         <th colspan="2"><span id="e"></span>E</th>
     </tr>
     <tr><td>Egypt</td><td>The Shantay Pass Desert</td></tr>
-    <!-- <tr><td>Elven Mist</td><td>Somewhere in Isafdar</td></tr> -->
+    <tr><td>Elven Mist</td><td>Somewhere in Isafdar</td></tr>
     <tr><td>Emotion</td><td>Inside Gnome Maze</td></tr>
     <tr><td>Emperor</td><td>Around Melzars Maze</td></tr>
     <tr><td>Escape</td><td>In the Hellhounds dungeon East of Ardougne (Family Crest)</td></tr>
-    <!-- <tr><td>Everywhere</td><td>West of the South entrance to Prifddinas</td></tr> -->
+    <tr><td>Everywhere</td><td>West of the South entrance to Prifddinas</td></tr>
     <tr><td>Expanse</td><td>West of the split off of NE Lumbridge farmland to Varrock and North Al Kharid entrance also around the magic axe hut in the Wilderness</td></tr>
     <tr><td>Expecting</td><td>West of the Khazard Battlefield at the Zamorak site</td></tr>
     <tr><td>Expedition</td><td>Observatory dungeon</td></tr>
-    <!-- <tr><td>Exposed</td><td>Near the Coal-Tar swamp in Isafdar</td></tr> -->
+    <tr><td>Exposed</td><td>Near the Coal-Tar swamp in Isafdar</td></tr>
     <tr>
         <th colspan="2"><span id="f"></span>F</th>
     </tr>
     <tr><td>Fairie</td><td>West Zanaris</td></tr>
-    <!-- <tr><td>Faithless</td><td>Chaos Altar Temple at Level 12 Wilderness</td></tr> -->
+    <tr><td>Faithless</td><td>Chaos Altar Temple at Level 12 Wilderness</td></tr>
     <tr><td>Fanfare</td><td>North Falador and by East Falador Bank</td></tr>
     <tr><td>Fanfare2</td><td>East Karamja Jungle Shipyard</td></tr>
     <tr><td>Fanfare3</td><td>Port Khazard</td></tr>
@@ -129,7 +129,7 @@ Quick Links<br>
     <tr><td>Fishing</td><td>Catherby Fishing Spots and at the Fishing Platform</td></tr>
     <tr><td>Flute Salad</td><td>Lumbridge West Farmlands</td></tr>
     <tr><td>Forbidden</td><td>Low level Wilderness north of Varrock Lumber Yard</td></tr>
-    <!-- <tr><td>Forest</td><td>Past the first leaf trap in the Elvin area</td></tr> -->
+    <tr><td>Forest</td><td>Past the first leaf trap in the Elvin area</td></tr>
     <tr><td>Forever</td><td>Around the Cooking Guild and around the KBD entrance cage and along the Member wilderness fence and at Edgeville</td></tr>
     <!-- <tr><td>Frogland</td><td>When you get the frog random event, ignore them and get teleported to the place where this plays</td></tr> -->
     <!-- <tr><td>Fruits de Mer</td><td>Fishing Platform (Sea Slug quest)</td></tr> -->
@@ -184,19 +184,19 @@ Quick Links<br>
     <tr>
         <th colspan="2"><span id="l"></span>L</th>
     </tr>
-    <!-- <tr><td>Lair</td><td>The Shade Tombs of Mort'ton</td></tr> -->
+    <tr><td>Lair</td><td>The Shade Tombs of Mort'ton</td></tr>
     <!-- <tr><td>La Mort</td><td>Death Altar</td></tr> -->
     <tr><td>Landlubber</td><td>SouthWest of Brimhaven near the Gold rocks</td></tr>
     <tr><td>Lasting</td><td>Around Ranging Guild and Hemenster</td></tr>
-    <!-- <tr><td>Legend</td><td>Northwest of Sinclair Mansion</td></tr> -->
+    <tr><td>Legend</td><td>Northwest of Sinclair Mansion</td></tr>
     <tr><td>Legion</td><td>Barbarian Outpost and the Wilderness Bandit Camp</td></tr>
-    <!-- <tr><td>Lighthouse</td><td>At the Lighthouse north of Barbarian Outpost</td></tr> -->
+    <tr><td>Lighthouse</td><td>At the Lighthouse north of Barbarian Outpost</td></tr>
     <tr><td>Lightness</td><td>Around the Charred Crater West of the Wilderness Castle</td></tr>
     <tr><td>Lightwalk</td><td>Catherby and southwest</td></tr>
     <tr><td>Lonesome</td><td>Prison Camp Mine in the Tourist Trap Desert</td></tr>
     <tr><td>Long Ago</td><td>Peninsula East of Yanille that has the gnome (Hazelmere) on it</td></tr>
     <tr><td>Long Way Home</td><td>Around Rimmington</td></tr>
-    <!-- <tr><td>Lost Soul</td><td>South of the Elf tracker in the Elf area</td></tr> -->
+    <tr><td>Lost Soul</td><td>South of the Elf tracker in the Elf area</td></tr>
     <tr><td>Lullaby</td><td>Northwest of the Sinclair Mansion and at some of the Digsite</td></tr>
     <tr>
         <th colspan="2"><span id="m"></span>M</th>
@@ -212,7 +212,7 @@ Quick Links<br>
     <tr><td>Medieval</td><td>By the mostly Member's Area of Varrock</td></tr>
     <tr><td>Mellow</td><td>Between Dwarf base and Fishing Guild</td></tr>
     <!-- <tr><td>Melodrama</td><td>Castle Wars waiting room</td></tr> -->
-    <!-- <tr><td>Meridian</td><td>In the Troll Stronghold, the cells of Mad Eadgar and Godric's cells</td></tr> -->
+    <tr><td>Meridian</td><td>In the Troll Stronghold, the cells of Mad Eadgar and Godric's cells</td></tr>
     <tr><td>Miles Away</td><td>Around the Crafting Guild</td></tr>
     <tr><td>Miracle Dance</td><td>Mind Rune Altar</td></tr>
     <tr><td>Monarch Waltz</td><td>Sinclair Mansion area</td></tr>
@@ -232,14 +232,14 @@ Quick Links<br>
         <th colspan="2"><span id="o"></span>O</th>
     </tr>
     <tr><td>Oriental</td><td>In the Temple of Ah-Za-Rhoom in Karamja Jungle</td></tr>
-    <!-- <tr><td>Out of the Deep</td><td>Dungeon underneath the Lighthouse</td></tr> -->
-    <!-- <tr><td>Overpass</td><td>The secret way into the elven nation</td></tr> -->
+    <tr><td>Out of the Deep</td><td>Dungeon underneath the Lighthouse</td></tr>
+    <tr><td>Overpass</td><td>The secret way into the elven nation</td></tr>
     <tr><td>Overture</td><td>Seers Village</td></tr>
     <tr>
         <th colspan="2"><span id="p"></span>P</th>
     </tr>
     <tr><td>Parade</td><td>By the Jolly Boar Inn</td></tr>
-    <!-- <tr><td>Pirates of Peril</td><td>Pirate House in Member's Wilderness</td></tr> -->
+    <tr><td>Pirates of Peril</td><td>Pirate House in Member's Wilderness</td></tr>
     <tr><td>Principality</td><td>Burthorpe</td></tr>
     <tr>
         <th colspan="2"><span id="q"></span>Q</th>
@@ -253,7 +253,7 @@ Quick Links<br>
     <tr><td>Regal2</td><td>At the Rogues Hideout in Member's Wilderness</td></tr>
     <tr><td>Reggae</td><td>Jungle West of Brimhaven and East Kharazi Jungle</td></tr>
     <tr><td>Reggae2</td><td>East Karamja on East side of the river</td></tr>
-    <!-- <tr><td>Relleka</td><td>In the Viking town of Relleka</td></tr> -->
+    <tr><td>Relleka</td><td>In the Viking town of Relleka</td></tr>
     <tr><td>Righteousness</td><td>In the Law Rune Altar</td></tr>
     <tr><td>Riverside</td><td>Mining area east of Ardougne</td></tr>
     <tr><td>Royale</td><td>Black Knight Hideout in Taverley Dungeon</td></tr>
@@ -262,11 +262,11 @@ Quick Links<br>
         <th colspan="2"><span id="s"></span>S</th>
     </tr>
     <tr><td>Sad Meadow</td><td>East section of West Ardougne and the Wilderness Ice plateau</td></tr>
-    <!-- <tr><td>Saga</td><td>Northwest of Sinclair Mansion</td></tr> -->
+    <tr><td>Saga</td><td>Northwest of Sinclair Mansion</td></tr>
     <tr><td>Scape Cave</td><td>Varrock Sewers and part of Edgeville Dungeon</td></tr>
     <tr><td>Scape Main</td><td>Login screen</td></tr>
     <tr><td>Scape Sad1</td><td>Around the Greater Demon ruins and the Hazeel Sewers</td></tr>
-    <!-- <tr><td>Scape Scared</td><td>Automatic song, it's from Halloween 2004 title screen</td></tr> -->
+    <tr><td>Scape Scared</td><td>Automatic song, it's from Halloween 2004 title screen</td></tr>
     <tr><td>Scape Soft</td><td>On the trails north of Falador</td></tr>
     <tr><td>Scape Wild1</td><td>North of the Chaos Altar and west of the Demon Ruins also at the level 22 Wilderness Death Crater</td></tr>
     <tr><td>Sea Shanty</td><td>Karamja around the Banana Plantation</td></tr>
@@ -287,8 +287,8 @@ Quick Links<br>
     <tr><td>Starlight</td><td>In the Blurite Mine</td></tr>
     <tr><td>Still Night</td><td>North of Lumbridge by the North enterance into Al Kharid</td></tr>
     <tr><td>Stratosphere</td><td>Cosmic Rune Altar</td></tr>
-    <!-- <tr><td>Sunburn</td><td>West of the Desert Mining Camp</td></tr> -->
-    <!-- <tr><td>Superstition</td><td>Legends Quest Cave in the Kharazi Jungle</td></tr> -->
+    <tr><td>Sunburn</td><td>West of the Desert Mining Camp</td></tr>
+    <tr><td>Superstition</td><td>Legends Quest Cave in the Kharazi Jungle</td></tr>
     <!-- <tr><td>Suspicious</td><td>During Monkey Madness Quest cutscenes</td></tr> -->
     <tr>
         <th colspan="2"><span id="t"></span>T</th>
@@ -297,14 +297,14 @@ Quick Links<br>
     <!-- <tr><td>Technology</td><td>Gnome Glider Hangar (Monkey Madness quest)</td></tr> -->
     <!-- <tr><td>Temple</td><td>Beneath the Ape Atoll Monkey's Temple</td></tr> -->
     <tr><td>The Desert</td><td>West part of Shantay Pass Desert</td></tr>
-    <!-- <tr><td>The Navigator</td><td>Swensen's Portal Maze in Relleka</td></tr> -->
+    <tr><td>The Navigator</td><td>Swensen's Portal Maze in Relleka</td></tr>
     <tr><td>The Shadow</td><td>On Crandor Isle</td></tr>
     <tr><td>The Tower</td><td>Chaos Druid Tower</td></tr>
     <tr><td>Theme</td><td>Around the Dwarf base with the Coal Trucks and in Baxtorian Waterfall cave</td></tr>
     <!-- <tr><td>Throne of the Demon</td><td>Golem Quest Portal area.</td></tr> -->
     <tr><td>Time Out</td><td>Random Event Maze</td></tr>
     <!-- <tr><td>Time to Mine</td><td>Using the golden cannonball in "Between a rock..."</td></tr> -->
-    <!-- <tr><td>Tiptoe</td><td>Draynor Manor basement where Count Draynor's coffin is</td></tr> -->
+    <tr><td>Tiptoe</td><td>Draynor Manor basement where Count Draynor's coffin is</td></tr>
     <tr><td>Tomorrow</td><td>By the Blurite Mine entrance</td></tr>
     <tr><td>Trawler</td><td>In the Trawler boat</td></tr>
     <tr><td>Trawler Minor</td><td>When you sink on the Trawler</td></tr>
@@ -341,16 +341,16 @@ Quick Links<br>
     <tr><td>Wander</td><td>South of Falador Farmlands</td></tr>
     <tr><td>Waterfall</td><td>Close to Baxtorian Waterfall and at Glarial's Tomb</td></tr>
     <tr><td>Waterlogged</td><td>Midway south in Mort Myre</td></tr>
-    <!-- <tr><td>Well of Voyage</td><td>In the well of voyage</td></tr> -->
+    <tr><td>Well of Voyage</td><td>In the well of voyage</td></tr>
     <tr><td>Wilderness2</td><td>West of the Red Spider Ruins in Wilderness</td></tr>
     <tr><td>Wilderness3</td><td>South of the Red Spider Ruins in Wilderness</td></tr>
     <tr><td>Wilderness4</td><td>The Graveyard north of the West Wilderness Ruins</td></tr>
-    <!-- <tr><td>Wildwood</td><td>Level 12 Wilderness north of the Chaos Altar</td></tr> -->
+    <tr><td>Wildwood</td><td>Level 12 Wilderness north of the Chaos Altar</td></tr>
     <tr><td>Witching</td><td>South of the Wilderness Poison Wood</td></tr>
     <tr><td>Wolf Mountain</td><td>At the Ice Plateau South of Red Dragon Isle and also at the Chaos Marsh</td></tr>
     <tr><td>Wonder</td><td>Around the level 3 Wilderness Dark Wizards</td></tr>
     <tr><td>Wonderous</td><td>Around the North entrance to Ardougne</td></tr>
-    <!-- <tr><td>Woodland</td><td>At the Elf Camp in Isafdar</td></tr> -->
+    <tr><td>Woodland</td><td>At the Elf Camp in Isafdar</td></tr>
     <tr><td>Workshop</td><td>East Falador and Mining Guild above ground area</td></tr>
     <tr>
         <th colspan="2"><span id="y"></span>Y</th>
