06db4d416178bc00d3f4aa20a0083f3902cb692f
Collin_M <24929130+Collin-M@users.noreply.github.com>
2026-06-01 18:10:31 -0500

    sort coord clues and fix wildy level on one step

--- pages/clueguides/guides/coordinates.php
+++ pages/clueguides/guides/coordinates.php
@@ -36,6 +36,18 @@ In order to aquire a sextant, watch, and chart, you first must have a coordinate
       side of the Lonely Willow Tree</td>
   </tr>
   <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0013south.png">00 deg 13 min South<br>
+      13 deg 58 min East</a>
+    </td>
+    <td>On Karamja between two small lakes, just past the mud path by the banana plantation.</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0018south.png">00 deg 18 min South<br>
+      09 min 28 deg East</a>
+    </td>
+    <td>South-west of Brimhaven, two paces South of the Northwestern most gold rock</td>
+  </tr>
+  <tr>
     <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0030north.png">00 deg 30 min North<br>
       24 deg 16 min East</a>
     </td>
@@ -100,11 +112,23 @@ In order to aquire a sextant, watch, and chart, you first must have a coordinate
       the 2 most Eastern ponds.</td>
   </tr>
   <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0705north.png">07 deg 05 min North<br>
+      30 deg 56 min East</a>
+    </td>
+    <td>In Mort Myre swamp, substantially Northwest of the island where the Nature Spirit is; dig near a tree</td>
+  </tr>
+  <tr>
     <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0733north.png">07 deg 33 min North<br>
       15 min 00 deg East</a>
     </td>
     <td>Lady of the Lake, SouthWest of the gate leading to Taverley - Dig on top 
       of some leaves</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0833north.png">08 deg 33 min North<br>
+      01 min 39 deg West</a>
+    </td>
+    <td>South-west corner of the Gnome Stronghold, cross a bridge to the terrorbird pen, exit the East-northeast gate of the pen and dig near the water.</td>
   </tr>
   <tr>
     <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0933north.png">09 deg 33 min North<br>
@@ -120,40 +144,16 @@ In order to aquire a sextant, watch, and chart, you first must have a coordinate
     <td>It's Northwest of Canifis (the werewolf town) in the swamps, just above the river portal (must have completed Priest in Peril quest to get there). Go North to gate to new area, proceed slightly East. The spot is on the North side of the path, South of some large roots.</td>
   </tr>
   <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0705north.png">07 deg 05 min North<br>
-      30 deg 56 min East</a>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1105north.png">11 deg 05 min North<br>
+      00 deg 45 min West</a>
     </td>
-    <td>In Mort Myre swamp, substantially Northwest of the island where the Nature Spirit is; dig near a tree</td>
+    <td>Between two swamps Northwest of the Grand Tree</td>
   </tr>
   <tr>
     <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1141north.png">11 deg 41 min North<br>
       14 deg 58 min East</a>
     </td>
     <td>An area surrounded by a fence next to the pub in Burthorpe</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0013south.png">00 deg 13 min South<br>
-      13 deg 58 min East</a>
-    </td>
-    <td>On Karamja between two small lakes, just past the mud path by the banana plantation.</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0018south.png">00 deg 18 min South<br>
-      09 min 28 deg East</a>
-    </td>
-    <td>South-west of Brimhaven, two paces South of the Northwestern most gold rock</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0833north.png">08 deg 33 min North<br>
-      01 min 39 deg West</a>
-    </td>
-    <td>South-west corner of the Gnome Stronghold, cross a bridge to the terrorbird pen, exit the East-northeast gate of the pen and dig near the water.</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1105north.png">11 deg 05 min North<br>
-      00 deg 45 min West</a>
-    </td>
-    <td>Between two swamps Northwest of the Grand Tree</td>
   </tr>
 </table>
 <hr>
@@ -166,6 +166,95 @@ In order to aquire a sextant, watch, and chart, you first must have a coordinate
     <th>Location:</th>
   </tr>
   <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0000north.png">00 deg 00 min North<br>
+      07 deg 13 min West</a>
+    </td>
+    <td>** Regicide area, Northeast of Tyras Camp (near the entrance to his camp, before the thick forest pass) past a stick trap, dig near the stone towards the back of the pond.</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0124north.png">01 deg 24 min North<br>
+      08 deg 05 min West</a>
+    </td>
+    <td>In Regicide area, go in the direction North of the tracker heading to the elves past the leaf trap, look for a path to the West of the leaf trap, there is a stick trap, South of the stick trap dig to the West of the first tree. The Saradomin wizard will appear, so be prepared!</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0246north.png">02 deg 46 min North<br>
+      29 deg 11 min East</a>
+    </td>
+    <td>Al Kharid obstacle Duel Arena. You must challenge a player and set it to obstacles. The place where to do could be described as, near the corner of an 'L' shaped wall with a vertical line attached to the bottom</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0345south.png">03 deg 45 min South<br>
+      22 deg 45 min East</a>
+    </td>
+    <td>** In the Bedabin Encampment, located in the Desert South of Al Kharid; digging spot is North of the small pond near the guarded tent. A Saradomin wizard will attack!</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0403south.png">04 deg 03 min South<br>
+      03 deg 11 min East</a>
+    </td>
+    <td>In the Ogre city of Gu'Tanoth. You must have completed the Watchtower Quest to have access to this one</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0405south.png">04 deg 05 min South<br>
+      04 deg 24 min East</a>
+    </td>
+    <td>** On the Ogre island South of Yanille.<br>To gain access to it, you must go through the tunnel South of the ogre city.<br> The clue is on the far East side of the island, inbetween a spear wall and a grouping of three roots.<br>A Saradomin Wizard (Level 108) will spawn and attack you. Kill it and dig again.</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0416south.png">04 deg 16 min South<br>
+      16 deg 16 min East</a>
+    </td>
+    <td>** Inside the Karamja Shipyard, Southernmost house.  Dig at the window between 2 bookshelves and a Level 108 Saradomin Wizard will attack!  (See the Grand Tree Quest Guide)</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0537north.png">05 deg 37 min North<br>
+      31 deg 15 min East</a>
+    </td>
+    <td>Right behind the Nature Spirit's tree in Mort Myre swamp</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0550south.png">05 deg 50 min South<br>
+      10 deg 05 min East</a>
+    </td>
+    <td>On Cairn Isle, by the Saradomin wizard</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0611south.png">06 deg 11 min South<br>
+      15 deg 07 min East</a>
+    </td>
+    <td>East part of Karamja just North of the stepping stones you can balance on which leads to a Jungle Forester</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0743south.png">07 deg 43 min South<br>
+        12 min 26 deg East</a>
+    </td>
+    <td>** In the Kharazi Jungle. You must have started the Legends Quest in order to get in.</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0803north.png">08 deg 03 min North<br>
+      31 min 16 deg East</a>
+    </td>
+    <td>In the Mort Myre swamp. Almost directly South of the gate leading into it. Head down, it was the second lake South of the gates.</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0805south.png">08 deg 05 min South<br>
+      15 deg 56 min East</a>
+    </td>
+    <td>** The Southeastern corner of Kharazi jungle (Legends Quest area), dig at the brown spot.</td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0826south.png">08 deg 26 min South<br>
+      10 min 28 deg East</a></td>
+    <td>** In the Kharazi Jungle, all the way South from the cave you did legends in, dig near a corpse, near the shore.</td>
+  </tr>
+  <tr>
+   <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1603north.png">16 deg 03 min North<br>
+      14 deg 07 min East</a>
+    </td>
+    <td>Just above the entrance to Mad Eadgar's cave on Trollheim mountain</td>
+  </tr>
+  <tr>
     <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1607north.png">16 deg 07 min North<br>
       22 deg 45 min East</a>
     </td>
@@ -174,11 +263,23 @@ In order to aquire a sextant, watch, and chart, you first must have a coordinate
       next to it. Dig directly North of it to find the casket. </td>
   </tr>
   <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1631north.png">16 deg 31 min North<br>
+      12 deg 54 min East</a>
+    </td>
+    <td>** Northern part of Troll Stronghold, 3 steps West of Mushroom the sleeping troll. A Saradomin wizard will attack!</td>
+  </tr>
+  <tr>
     <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1643north.png">16 deg 43 min North<br>
       19 deg 13 min East</a>
     </td>
     <td>* At the Bandits Camp in the level 23 wilderness - Through the house and 
       outside with the rats, in the corner by the lake, as much East as possible, in the broken wall. </td>
+  </tr>
+  <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1850north.png">18 deg 50 min North<br>
+      20 deg 26 min East</a>
+    </td>
+    <td>* Just West of the Easternmost rocks of Hobgoblin Mine in Wilderness.</td>
   </tr>
   <tr>
     <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1900north.png">19 deg 00 min North<br>
@@ -227,6 +328,12 @@ In order to aquire a sextant, watch, and chart, you first must have a coordinate
       dig on the most Northeast pentagram (the red star on the ground).</td>
   </tr>
   <tr>
+    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/2424north.png">24 deg 24 min North<br>
+      26 deg 24 min East</a>
+    </td>
+    <td>Level 53 Wilderness at the Rogues Castle. On the inside, the most Northeast point.</td>
+  </tr>
+  <tr>
     <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/2456north.png">24 deg 56 min North<br>
       22 deg 28 min East</a>
     </td>
@@ -252,113 +359,6 @@ In order to aquire a sextant, watch, and chart, you first must have a coordinate
        23 deg 24 min East</a>
       </td>
     <td>* Northeast of Mage Arena, North of the building with the magic axes 2 spaces West of the back door. </td>
-  </tr>
-  <tr>
-   <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1603north.png">16 deg 03 min North<br>
-      14 deg 07 min East</a>
-    </td>
-    <td>Just above the entrance to Mad Eadgar's cave on Trollheim mountain</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1631north.png">16 deg 31 min North<br>
-      12 deg 54 min East</a>
-    </td>
-    <td>** Northern part of Troll Stronghold, 3 steps West of Mushroom the sleeping troll. A Saradomin wizard will attack!</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0550south.png">05 deg 50 min South<br>
-      10 deg 05 min East</a>
-    </td>
-    <td>On Cairn Isle, by the Saradomin wizard</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0826south.png">08 deg 26 min South<br>
-      10 min 28 deg East</a></td>
-    <td>** In the Kharazi Jungle, all the way South from the cave you did legends in, dig near a corpse, near the shore.</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0743south.png">07 deg 43 min South<br>
-        12 min 26 deg East</a>
-    </td>
-    <td>** In the Kharazi Jungle. You must have started the Legends Quest in order to get in.</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0805south.png">08 deg 05 min South<br>
-      15 deg 56 min East</a>
-    </td>
-    <td>** The Southeastern corner of Kharazi jungle (Legends Quest area), dig at the brown spot.</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0416south.png">04 deg 16 min South<br>
-      16 deg 16 min East</a>
-    </td>
-    <td>** Inside the Karamja Shipyard, Southernmost house.  Dig at the window between 2 bookshelves and a Level 108 Saradomin Wizard will attack!  (See the Grand Tree Quest Guide)</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0611south.png">06 deg 11 min South<br>
-      15 deg 07 min East</a>
-    </td>
-    <td>East part of Karamja just North of the stepping stones you can balance on which leads to a Jungle Forester</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0537north.png">05 deg 37 min North<br>
-      31 deg 15 min East</a>
-    </td>
-    <td>Right behind the Nature Spirit's tree in Mort Myre swamp</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0803north.png">08 deg 03 min North<br>
-      31 min 16 deg East</a>
-    </td>
-    <td>In the Mort Myre swamp. Almost directly South of the gate leading into it. Head down, it was the second lake South of the gates.</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0403south.png">04 deg 03 min South<br>
-      03 deg 11 min East</a>
-    </td>
-    <td>In the Ogre city of Gu'Tanoth. You must have completed the Watchtower Quest to have access to this one</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0405south.png">04 deg 05 min South<br>
-      04 deg 24 min East</a>
-    </td>
-    <td>** On the Ogre island South of Yanille.<br>To gain access to it, you must go through the tunnel South of the ogre city.<br> The clue is on the far East side of the island, inbetween a spear wall and a grouping of three roots.<br>A Saradomin Wizard (Level 108) will spawn and attack you. Kill it and dig again.</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0345south.png">03 deg 45 min South<br>
-      22 deg 45 min East</a>
-    </td>
-    <td>** In the Bedabin Encampment, located in the Desert South of Al Kharid; digging spot is North of the small pond near the guarded tent. A Saradomin wizard will attack!</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0246north.png">02 deg 46 min North<br>
-      29 deg 11 min East</a>
-    </td>
-    <td>Al Kharid obstacle Duel Arena. You must challenge a player and set it to obstacles. The place where to do could be described as, near the corner of an 'L' shaped wall with a vertical line attached to the bottom</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/1850north.png">18 deg 50 min North<br>
-      20 deg 26 min East</a>
-    </td>
-    <td>* Just West of the Easternmost rocks of Hobgoblin Mine in Wilderness.</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/2424north.png">24 deg 24 min North<br>
-      26 deg 24 min East</a>
-    </td>
-    <td>Level 50 Wilderness at the Rogues Castle. On the inside, the most Northeast point.</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0000north.png">00 deg 00 min North<br>
-      07 deg 13 min West</a>
-    </td>
-    <td>** Regicide area, Northeast of Tyras Camp (near the entrance to his camp, before the thick forest pass) past a stick trap, dig near the stone towards the back of the pond.</td>
-  </tr>
-  <tr>
-    <td><a href="#" class="image-link" data-image="img/clue_guides/coordinates/0124north.png">01 deg 24 min North<br>
-      08 deg 05 min West</a>
-    </td>
-    <td>In Regicide area, go in the direction North of the tracker heading to the elves past the leaf trap, look for a path to the West of the leaf trap, there is a stick trap, South of the stick trap dig to the West of the first tree. The Saradomin wizard will appear, so be prepared!</td>
   </tr>
 </table><br>
 * For Wilderness Clues: A Zamorak wizard will appear. Kill him and then dig again to get a casket.<br>
